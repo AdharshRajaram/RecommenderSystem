8063c9e7a547b1df606630d57c56119b2c30a24a
Anandakrishnan <adharsh.rajaram@gmail.com>
2017-01-03 10:33:28 +0530

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,23 +1,31 @@
 library(shiny)
-library(Matrix)
-library(recommenderlab)
 shinyUI(fluidPage(
   titlePanel("Recommender Systems"),
-  sidebarLayout(
-    sidebarPanel(
-      fileInput("trainF","Upload Your Historical Ratings Data:"),
-      #uiOutput("ClearFeature"),
-      fileInput("FeatureF","Upload Item Features(Required only for content-based)"),
-      selectInput("Method","Type of Recommender System:",c("Auto","IBCF","UBCF"),selected = "Auto")
+  fluidRow(
+    column(3,
+     wellPanel(
+       uiOutput("LoadInput"),
+       actionButton("Load",label="Load")
+     ),  
+     wellPanel(
+       uiOutput("InputTrain"),  
+       uiOutput("SaveUI"), 
+       uiOutput("InfoSavedModel")
+     )
     ),
-    mainPanel(
-      tabsetPanel(type = "tabs",
-        tabPanel("EDA",textOutput("default0"),textOutput("Nusers"),textOutput("Nitems"),
-                 textOutput("TRating"),plotOutput("RatingDist"),plotOutput("UserDist"),
-                 plotOutput("AvgRating"),plotOutput("ROC"),plotOutput("Metric")),
-        tabPanel("Singe User",textOutput("default1")),
-        tabPanel("All Users",textOutput("default2"))
+    column(9,mainPanel(
+      tabsetPanel(
+        type = "tabs",
+        tabPanel(
+          "EDA",
+          uiOutput("TrainEDA")
+          ),
+          tabPanel(
+           "Recommended Items",
+            uiOutput("TrainRec")
+          )
       )
     )
+   )  
   )
 ))
